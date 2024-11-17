73358431edf48c939e9a87ac11bf833dec76187c
Pascal Thomet <pthomet@gmail.com>
2024-11-17 19:26:49 +0100

    bindings with nanobind

---
+++ blah.cpp
@@ -0,0 +1,7 @@
+#include <vector>   // Used *once* to make the FontAtlas api accessible on python
+#include <tuple>
+#include <optional>
+#include <functional>
+#include <string>
+#include <map>
+std::optional<int> v;
